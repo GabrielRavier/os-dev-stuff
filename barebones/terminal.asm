dc29db873b670dd787549aacb3ad943098750b62
Gabriel Ravier <gabravier@gmail.com>
2019-04-05 08:06:21 +0200

    Added impl for terminalSetColor and terminalPutEntryAt

--- barebones/terminal.asm
+++ barebones/terminal.asm
@@ -1,4 +1,6 @@
 global _terminalInitialize
+global _terminalSetColor
+global _terminalPutEntryAt
 
 segment .bss align=16
 
@@ -18,4 +20,66 @@ segment .text align=16
 
     align 16
 terminalInitialize:
+    push ebx
+    push ebp
+    
+    xor edx, edx
+    xor ecx, ecx
+
+    mov ebp, 0x720
+    mov [terminalRow], edx
+    xor eax, eax
+    mov [terminalColumn], edx
+    mov byte [terminalColor], 7
+    mov dword [terminalBuffer], 0xB8000
+
+.loop:
+    mov ebx, edx
+
+.loop2:
+    mov word [0xB8000 + eax + ebx * 4], bp
+    mov word [0xB8002 + eax + ebx * 4], bp
+
+    inc ebx
+    cmp ebx, 40
+    jb .loop2
+
+    inc ecx
+    add eax, 0xA0
+    cmp ecx, 0x19
+    jb .loop
+
+    pop ebp
+    pop ebx
+    ret
+
+
+
+
+
+    align 16
+_terminalSetColor:
+    mov eax, [esp + 4]
+    mov [terminalColor], al
+    ret
+
+
+
+
+
+    align 16
+_terminalPutEntryAt:
+    mov ecx, [esp + 16]
+    mov edx, [esp + 12]
+    movzx eax, byte [esp + 4]
+    
+    lea ecx, [ecx + ecx * 4]
+    shl edx, 8
+    shl ecx, 4
+    or edx, eax
+
+    mov eax, [terminalBuffer]
+    add ecx, [esp + 12]
+    mov [eax + ecx * 2], dx
+    ret
     
