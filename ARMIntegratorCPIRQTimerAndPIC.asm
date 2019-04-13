3e669e004e053696f6f92b545885ea8cb180a843
Gabriel Ravier <gabravier@gmail.com>
2019-04-13 17:50:59 +0200

    Started ARMIntegratorCPIRQTimerAndPIC
    Signed-off-by: Gabriel Ravier <gabravier@gmail.com>

---
+++ ARMIntegratorCPIRQTimerAndPIC.asm
@@ -0,0 +1,43 @@
+
+	code32
+
+	format ELF
+	public _entry
+	public _kSerDbgPutc
+	public _arm4CpsrGet
+
+section '.text' executable align 16
+
+_entry:
+	mov sp, #0x4000
+	mov r1, #0x16000000
+	mov r2, #65
+	str r2, [r1]
+	bl _start
+
+
+
+
+
+_kSerDbgPutc:
+	mov r2, #0x16000000
+
+.loop:
+	ldr r3, [r2, #24]
+	tst r3, #32
+	bne .loop
+
+	cmp r0, #10
+	str r0, [r2]
+	bxne lr
+
+	mov r0, #13
+	b .loop
+
+
+
+
+
+_arm4CpsrGet:
+	mrs r0, cpsr
+	bx lr
--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ nasm -f elf32 8259PIC.asm &
 nasm -f elf32 AHCI.asm &
 nasm -f elf32 AMDPCNET.asm &
 nasm -f elf32 APIC.asm &
-fasmarm ARMIntegratorCPBareBonesPutcPuts.asm
+fasmarm ARMIntegratorCPBareBonesPutcPuts.asm &
+fasmarm ARMIntegratorCPIRQTimerAndPIC.asm
